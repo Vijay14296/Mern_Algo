394e43ea776588deb0a2fa7478f267a0c0fefbab
Vijay <vijaydharani269@gmail.com>
2025-08-10 18:30:25 +0530

    Frontend changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",  // Your backend URL
+  baseURL: import.meta.env.VITE_API_URL + "/api"
 });
+
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
